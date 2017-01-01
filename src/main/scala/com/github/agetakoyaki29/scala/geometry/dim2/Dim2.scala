25ab1e6bf185a54dfbcff40c358927f939309cff
Your Name <you@example.com>
2017-01-02 05:09:55 +0900

    make Dim2Factory

--- src/main/scala/com/github/agetakoyaki29/scala/geometry/dim2/Dim2.scala
+++ src/main/scala/com/github/agetakoyaki29/scala/geometry/dim2/Dim2.scala
@@ -1,13 +1,25 @@
 package com.github.agetakoyaki29.scala.geometry.dim2
 
 import com.github.agetakoyaki29.scala.geometry.Delta._
+import scala.reflect.ClassTag
 
 
-object Dim2 {
-  class SimpleDim2(val x: Double, val y: Double) extends Dim2
+abstract class Dim2Factory[T <: Dim2 : ClassTag] {
+  def apply(x: Double, y: Double): T
+  def apply(op: Dim2): T = op match {
+    case t: T => t
+    case dim2 => clone(dim2)
+  }
+  def clone(dim2: Dim2): T = apply(dim2.x, dim2.y)
+}
+
+
+object Dim2 extends Dim2Factory[Dim2] {
+  class SimpleDim2 private[Dim2] (val x: Double, val y: Double) extends Dim2
   
   def apply(x: Double, y: Double): Dim2 = new SimpleDim2(x, y)
 }
+
 
 abstract class Dim2 {
   def x: Double
@@ -26,13 +38,13 @@ abstract class Dim2 {
 
   // ----
 
-//  def factory: Dim2Factory[_ <: Dim2] = Dim2
+  def factory: Dim2Factory[_ <: Dim2] = Dim2
 
   def isZero: Boolean = x==0 && y==0
   def isInfinite = x.isInfinite || y.isInfinite
 
-//  def mapD2(f: Double => Double): Dim2 = factory(f(x), f(y))
-  
+  def mapD2(f: Double => Double): Dim2 = factory(f(x), f(y))
+  def zipmap(op: Dim2)(f: (Double, Double) => Double): Dim2 = factory(f(x, op.x), f(y, op.y))
 
   // -- IndexedSeq --
 

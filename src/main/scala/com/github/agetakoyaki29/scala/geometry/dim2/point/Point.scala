4eb1ca958d833a17427843168f8e0737d833eb1d
Your Name <you@example.com>
2017-01-05 06:52:33 +0900

    add Point fff

--- src/main/scala/com/github/agetakoyaki29/scala/geometry/dim2/point/Point.scala
+++ src/main/scala/com/github/agetakoyaki29/scala/geometry/dim2/point/Point.scala
@@ -1,5 +1,7 @@
 package com.github.agetakoyaki29.scala.geometry.dim2.point
 
+import scala.reflect.ClassTag
+
 import com.github.agetakoyaki29.scala.sameret.SameRet
 import com.github.agetakoyaki29.scala.sameret.UpRet
 import com.github.agetakoyaki29.scala.geometry.dim2.Dim2
@@ -22,8 +24,27 @@ class Point protected (x: Double, y: Double) extends Vector(x, y) with Trans[Poi
   def distance(op: Point) = (this localize op).norm
   def distanceSqr(op: Point) = (this localize op).normSqr
 
-  def localize[T](trans: Trans[T]): T = trans localizedBy this
-  def unlocalize[T](trans: Trans[T]): T = trans unlocalizedBy this
+  // ---- use trans ----
+
+  // def localize[T](trans: Trans[T]): T = trans localizedBy this
+  def localize[A: ClassTag](any: A): A = any match {
+    case trans: Trans[A] => trans localizedBy this
+    case any => any
+  }
+  // def localize[A: ClassTag](seq: Seq[A]): Seq[A] = seq.map{localize[A](_)}
+
+  // def unlocalize[T](trans: Trans[T]): T = trans unlocalizedBy this
+  def unlocalize[A: ClassTag](any: A): A = any match {
+    case trans: Trans[A] => trans unlocalizedBy this
+    case any => any
+  }
+  // def unlocalize[A: ClassTag](seq: Seq[A]): Seq[A] = seq.map{unlocalize[A](_)}
+
+  def fff[T: ClassTag, R: ClassTag](f: T => R): T => R = f compose localize[T] andThen unlocalize[R]
+  // def fff[T: ClassTag, R: ClassTag](f: Seq[T] => R): Seq[T] => R = f compose localize[T] andThen unlocalize[R]
+  // def fff[T, R](f: T => R): T => R = f match {
+  //   case f: Trans[TT] => Trans[TR] =>
+  // }
 
   // ---- for trans ----
 

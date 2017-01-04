1bfe0fe8ff2acb4e24b0cd5577cccd72e3932b69
Your Name <you@example.com>
2017-01-05 05:48:33 +0900

    add TransSeq

--- src/main/scala/com/github/agetakoyaki29/scala/geometry/dim2/point/Trans.scala
+++ src/main/scala/com/github/agetakoyaki29/scala/geometry/dim2/point/Trans.scala
@@ -1,6 +1,14 @@
 package com.github.agetakoyaki29.scala.geometry.dim2.point
 
 
+object Trans {
+  implicit class TransSeq[A <: Trans[A]](it: Seq[A]) extends Trans[TransSeq[A]] {
+    def +(pt: Point): TransSeq[A] = it.map{_+pt}
+    def -(pt: Point): TransSeq[A] = it.map{_-pt}
+  }
+}
+
+
 trait Trans[Repr] {
   def +(pt: Point): Repr
   def -(pt: Point): Repr
